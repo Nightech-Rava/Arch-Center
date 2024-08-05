05018ca999488da802fb580806845456b3bb59bb
Electro Organic <sys.net.code.data@outlook.com>
2024-08-05 05:51:55 +0200

    Update arch-basic-commands.sh

--- arch-basic-commands.sh
+++ arch-basic-commands.sh
@@ -1,26 +1,5 @@
-
-Skip to content
-All gists
-Back to GitHub
-Sign in
-Sign up
-
-Instantly share code, notes, and snippets.
-@yufengwng
-yufengwng/arch_cheatsheet.txt
-Last active July 17, 2024 16:34
-
-Code
-Revisions 3
-Stars 81
-Forks 34
-Clone this repository at &lt;script src=&quot;https://gist.github.com/yufengwng/9cff3fc82403e3f3052d.js&quot;&gt;&lt;/script&gt;
-Arch Linux Commands Cheatsheet
-arch_cheatsheet.txt
-pacman
-======
-
-view logs: /var/log/pacman.log
+view logs
+# /var/log/pacman.log
 
 update system
 # pacman -Syu
@@ -46,15 +25,18 @@ remove package, its dependencies and config file backups
 clean old packages in cache
 # pacman -Sc
 
+======
 yaourt
 ======
 
 same as pacman
 
+=======
 systemd
 =======
 
-unit files: /usr/lib/systemd/system/ or /etc/systemd/system/
+unit files
+# /usr/lib/systemd/system/ or /etc/systemd/system/
 
 list running units
 $ systemctl
@@ -71,11 +53,12 @@ enable/disable a service at bootup
 reload systemd
 # systemctl daemon-reload
 
+==============================
 manual install of AUR packages
 ==============================
 
 update repositories
-# pacman -Sy
+$ pacman -Sy
 
 grab the package
 $ curl -O <url> (e.g. https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz)
@@ -89,6 +72,7 @@ $ cd <package>
 build and install
 $ makepkg -si
 
+=================
 java environments
 =================
 
